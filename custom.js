f3e9386dc73f05b53feed3529062bf6a0acece1d
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-04-04 14:36:29 -0400

    Added full functionality to RPS program
    Program now keeps track of score (up to 5) and
    declares a winner once 5 is reached. Then a restart
     button allows the user to play again.

--- custom.js
+++ custom.js
@@ -19,31 +19,44 @@ function getComputerChoice(){
     return computerChoice;
 }
 
+let pScore = 0;
+let cScore = 0;
+let tieScore = 0;
+
 function playGame(playerSelection, computerSelection){
     let results;
     if (playerSelection === "rock"){ // code for if player picked rock
         if  (computerSelection === "paper") {
             results = "You lose! Paper beats Rock.";
+            cScore++;
         } else if (computerSelection === "scissors") {
             results = "You Win! Rock beats Scissors.";
+            pScore++;
         } else {
             results = "It's a tie, you both picked Rock.";
+            tieScore++;
         }
     } else if (playerSelection === "paper") { // code for if player picked paper
         if  (computerSelection === "scissors") {
             results = "You lose! Scissors beats Paper.";
+            cScore++;
         } else if (computerSelection === "rock") {
             results = "You Win! Paper beats Rock.";
+            pScore++;
         } else {
             results = "It's a tie, you both picked Paper.";
+            tieScore++;
         }
     } else if (playerSelection ==="scissors"){ // code for if player picked scissors
         if  (computerSelection === "rock") {
+            cScore++;
             results = "You lose! Rock beats Scissors.";
         } else if (computerSelection === "paper") {
             results = "You Win! Scissors beats Paper.";
+            pScore++;
         } else {
             results = "It's a tie, you both picked Scissors.";
+            tieScore++;
         }
     } else { // code for if player did not enter rock, paper, or scissors
         results = "You did not pick rock, paper, or scissors. You automatically lost!"
@@ -52,15 +65,44 @@ function playGame(playerSelection, computerSelection){
 }
 
 const buttons = document.querySelectorAll("button");
-let result = ''
+const resultDisplay = document.querySelector("#resultsText");
+const playerChoice = document.querySelector("#playerChoice");
+const scoreCard = document.querySelector("#scoreCard");
+const computerChoice = document.querySelector("#computerChoice");
+const btnContainer = document.querySelector("#btnContainer");
+
+const playAgainBtn = document.createElement("button");
+playAgainBtn.textContent = "Play Again?";
+
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
 // and for each one we add a 'click' listener
     button.addEventListener("click", () => {
-        result = playGame(button.id, getComputerChoice());
-        console.log(result);
+        const playerBtn = button.id;
+        const computerSelection = getComputerChoice();
+        const result = playGame(playerBtn, computerSelection);
+        playerChoice.textContent = `Your Choice: ${playerBtn}`;
+        computerChoice.textContent = `Computer's Choice: ${computerSelection}`;
+        resultDisplay.textContent = result;
+        scoreCard.textContent = `Win: ${pScore} -- Tie: ${tieScore} -- Loss: ${cScore}`;
+
+        if (pScore == 5){
+            scoreCard.textContent = 'You beat the computer!';
+            document.querySelector("#rock").remove();
+            document.querySelector("#paper").remove();
+            document.querySelector("#scissors").remove();
+            document.querySelector("body").insertBefore(playAgainBtn, btnContainer);
+        } else if (cScore == 5){
+            scoreCard.textContent = 'The computer won this time.';
+            document.querySelector("#rock").remove();
+            document.querySelector("#paper").remove();
+            document.querySelector("#scissors").remove();
+            document.querySelector("body").insertBefore(playAgainBtn, btnContainer);
+        }
     });
 });
 
-const resultDisplay = document.createElement('div');
+playAgainBtn.addEventListener('click', () =>{
+    window.location.reload();
+})
